5f2df12367025b22c12749a23f0786ee52090b1b
lesquoyb <nouvelleaddresse@yahoo.fr>
2015-10-27 02:22:28 +0100

    Quelques réglages

--- main/main/Main.java
+++ main/main/Main.java
@@ -1,12 +1,12 @@
 package main;
 
+
 public class Main {
 
 	
 	public static void main(String[] args) throws Exception {
 		Robot r = new Robot();
-		r.init();
-		r.choisirScenario();
-		
+		//r.choisirScenario();
+		r.followLine(Robot.Green);
 	}
 }
--- main/main/Robot.java
+++ main/main/Robot.java
@@ -6,7 +6,6 @@ import lejos.hardware.motor.Motor;
 import lejos.hardware.motor.NXTRegulatedMotor;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.internal.ev3.EV3MotorPort;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 import lejos.utility.TextMenu;
@@ -15,43 +14,73 @@ import lejos.utility.TextMenu;
 
 public class Robot {
 
+	public static final ReadColor Red = new ReadColor(255, 0, 0);
+	public static final ReadColor Yellow = new ReadColor(255, 255, 0);
+	public static final ReadColor Green = new ReadColor(0, 255, 0);
+	public static final ReadColor Blue = new ReadColor(0, 0, 255);
 	
-	enum Color{Yellow, Green, Red};
 	
 	
 	private SampleProvider colorSampler;
+	private float[] colorSample;
 	private EV3ColorSensor colorSensor;
 	private void initColorSensor(){
 		colorSensor = new EV3ColorSensor(LocalEV3.get().getPort("S1"));
 		colorSampler = colorSensor.getRGBMode();
+		colorSample = new float[3];
 	}
+	
+	public ReadColor black = new ReadColor(50, 50, 50), white = new ReadColor(100,100,100);
 	private void calibrateBlackAndWhite(){
-		//TODO: flemme
+		white = new ReadColor(0, 0, 0);
+		black = new ReadColor(255, 255, 255);
+		ReadColor tmp;
+		for(int i = 0 ; i < 36 ; i++){
+			turn(10);
+			tmp = readColor();
+			if(tmp.isClearerThan(white)){
+				white = tmp;
+			}
+			else if (tmp.isDarkerThan(black)){
+				black = tmp;
+			}
+		}
+		//TODO: si on garde le noir comme la limite entre la bande blanche et la bande noire, il faut 
+		// l'incrémenter proportionnellement à white - black
 	}
+	public ReadColor readColor(){
+		colorSampler.fetchSample(colorSample, 0);
+		return new ReadColor(Math.min( (int) (colorSample[0]*255), 255), 
+							 Math.min( (int) (colorSample[1]*255), 255),
+							 Math.min( (int) (colorSample[2]*255), 255)); 
+	}
+	
+	
 	
 	private EV3GyroSensor gyro;
 	private SampleProvider gyroSampler;
+	private static float[] gyroSample;
 	private final NXTRegulatedMotor pivotRotation = Motor.B;
 	private final NXTRegulatedMotor motorRotation = Motor.A;
 	private void  initGyro(){
 		gyro = new EV3GyroSensor(LocalEV3.get().getPort("S2"));
 		gyroSampler =  gyro.getAngleMode();
-		sample = new float[1];
+		gyroSample = new float[1];
 	}
-	static float[] sample;
 	static final byte position_angle_dans_sample = 0;
 	static int nombre_de_degre_par_rotation_moteur = 61;
+	static final int rotation_minimale_pour_se_deplacer = 3;//TODO à régler à la main avec le robot final
 	private void calibrateGyro(){
 		gyro.reset();
 		motorRotation.rotate(360);
-		gyroSampler.fetchSample(sample, 0);
-		nombre_de_degre_par_rotation_moteur = (int)sample[position_angle_dans_sample];
+		gyroSampler.fetchSample(gyroSample, 0);
+		nombre_de_degre_par_rotation_moteur = (int)gyroSample[position_angle_dans_sample];
 	}
 	
 	
 	
 	public Robot(){
-		
+		init();
 	}
 
 	
@@ -68,28 +97,35 @@ public class Robot {
 	}
 	
 	public void calibrer(){
+		//Sérialiser les valeurs ?
 		calibrateBlackAndWhite();
+		System.out.println("black: " + black + "\n white: " +white);
+		Delay.msDelay(10000);
 		calibrateGyro();
 	}
 	
 	public void choisirScenario() throws Exception{
-		String[] menu = new String[]{"Rouge","Vert","Jaune", "Quitter"};
+		String[] menu = new String[]{"Rouge","Vert","Jaune", "Calibrer", "Quitter"};
 		TextMenu m = new TextMenu(menu);
 		int index = m.select();
-		Color selectedColor = null;
+		ReadColor selectedColor = null;
 		switch(menu[index]){
 			case "Rouge":
-				selectedColor = Color.Red;
+				selectedColor = Red;
 				break;
 				
 			case "Vert":
-				selectedColor = Color.Green;				
+				selectedColor = Green;				
 				break;
 				
 			case "Jaune":
-				selectedColor = Color.Yellow;
+				selectedColor = Yellow;
 				break;
-				
+			case "Calibrer":
+				m.quit();
+				calibrer();
+				choisirScenario();
+				return ;
 			case "Quitter":
 				m.quit();
 				return;
@@ -101,67 +137,51 @@ public class Robot {
 	}
 	
 	
-	private boolean colorDetected(int r, int g, int b, Color color) throws Exception{
-		switch(color){
-		//TODO: faire ça proprement
-		case Green:
-			return g > r + b;
-		case Red:
-			return r > g + b;
-		case Yellow:
-			return r + g/3 > b;
-			default:
-				throw new Exception("couleur non implémentée");
-		}
+	private boolean colorDetected(ReadColor read, ReadColor goal) throws Exception{
+		//TODO
+		return false;
 	}
 	
 	
-	private void followLine(Color color) throws Exception{
+	public void followLine(ReadColor color) throws Exception{
 
-		float[] sample = new float[3];
 		
-		int r, g, b, t;
-		int baseSpeed = 50;
+		int baseSpeed = 150;
 		
 		
-		double kp = 2; //TODO: tester différentes valeurs
-		double ki = 1; //TODO: idem
-		double kd = 0; //TODO: idem
+		double kp = baseSpeed/2; //TODO: tester différentes valeurs
+		double ki = 0; //TODO: idem
+		double kd = baseSpeed/5; //TODO: idem
 		
 		double virage;
 		
 
-		double error, sum_errors = 0, last_error = 0;
+		double error, sum_errors = 0, last_error = 0, delta_error;
 		
-		//ugly but who cares ?
-		colorSampler.fetchSample(sample, 0);
-		r = (int) (sample[0]*255);
-		g = (int) (sample[1]*255);
-		b = (int) (sample[2]*255);//TODO: ok, I care, I'll fix it someday
-		
+		ReadColor read = readColor();
 		
 		Motor.A.backward();
 		Motor.B.backward();
-		while( ! colorDetected(r, g, b, color) ){
-			colorSampler.fetchSample(sample, 0);
+		
+		Motor.A.setSpeed((int) (baseSpeed ));
+		Motor.B.setSpeed((int) (baseSpeed ));
+		
+		while( ! colorDetected(read, color) ){
 
-			r = (int) (sample[0]*255);
-			g = (int) (sample[1]*255);
-			b = (int) (sample[2]*255);
-			
-			t = (r+g+b)/3;
+			read = readColor();
 
-			//System.out.println(t);
+			error = (black.getAverage() - read.getAverage() )/ (double)(white.getAverage()- black.getAverage());
 			
-			error = t - 20;
 			sum_errors += error;
+			delta_error = error - last_error;
+			last_error = error;
 			
+			virage = kp * error + ki * sum_errors + kd * delta_error;
+
+			System.out.println(virage);
 			
-			
-			virage = kp * error + ki * sum_errors;
-			
-			Motor.A.setSpeed((int) (baseSpeed + virage));
-			Motor.B.setSpeed((int) (baseSpeed - virage));
+			Motor.A.setSpeed((int) Math.max(baseSpeed - virage,0));//TODO: ralentir dans les virages
+			Motor.B.setSpeed((int) Math.max(baseSpeed + virage,0));
 
 			Delay.msDelay(50);
 		}
@@ -177,15 +197,15 @@ public class Robot {
 		
 		//TODO: ne pas oublier de changer la rotation si on change de moteur pivot
 		angle %= 360;
-		System.out.println("angle demande: " + angle);
 		
 		gyro.reset();//ancrage
 		float mesure;
 		int relAngle;
 		do{
-			gyroSampler.fetchSample(sample, 0);
-			mesure = sample[position_angle_dans_sample];
+			gyroSampler.fetchSample(gyroSample, 0);
+			mesure = gyroSample[position_angle_dans_sample];
 			relAngle =  (int) ( (angle - mesure) * 100 / nombre_de_degre_par_rotation_moteur);
+			relAngle = (relAngle > 0 ) ? Math.max(relAngle, rotation_minimale_pour_se_deplacer) : Math.min(relAngle, -rotation_minimale_pour_se_deplacer); //Pour éviter de rester bloqué
 			pivotRotation.rotate( - relAngle, true);
 			Delay.msDelay(50);
 			//TODO: attention on pourrait rester bloqué, définir un mouvement minimum
